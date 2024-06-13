63dcd9a3a166ff4daa1802f24830e7a78af8689b
Arghya Chattapadhyay <arghyachatterjee984@gmail.com>
2024-06-13 20:09:08 +0530

    got the basic code working

--- frontend/src/components/FeedbackList.tsx
+++ frontend/src/components/FeedbackList.tsx
@@ -12,7 +12,7 @@ const FeedbackList: React.FC = () => {
   useEffect(() => {
     const fetchFeedbacks = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/feedback');
+        const response = await axios.get('http://localhost:2000/feedback');
         setFeedbacks(response.data);
       } catch (error) {
         console.error('Error fetching feedbacks', error);
